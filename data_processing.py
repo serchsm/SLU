b77f4e326adcc66742ff64fc88f524c12e15366b
serchsm <serchsm@users.nonreply.github.com>
2021-04-18 20:58:17 -0400

    "[WIP] Skeleton TransformerEncoder and read all transcripts"

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,24 @@
+import logging
 import pathlib
 import pandas as pd
+import tensorflow as tf
 
+tf.get_logger().setLevel('ERROR')
 pwd = pathlib.Path.cwd()
 
 train_data = pd.read_csv(pwd.joinpath('fluent_speech_commands_dataset', 'data', 'train_data.csv'))
 test_data = pd.read_csv(pwd.joinpath('fluent_speech_commands_dataset', 'data', 'test_data.csv'))
 validation_data = pd.read_csv(pwd.joinpath('fluent_speech_commands_dataset', 'data', 'valid_data.csv'))
-for file_path, transcript in zip(train_data['path'], train_data['transcription']):
-    print(f"{file_path}, {transcript}")
+
+all_transcripts = pd.concat([train_data, test_data, validation_data], ignore_index=True)
+
+for transcript in all_transcripts['transcription']:
+    print(f"{transcript}")
+
+fsc_transcripts = tf.data.Dataset.from_tensor_slices(all_transcripts['transcription'].str.lower().values)
+
+# for transcripts in fsc_transcripts.take(5):
+#     print(f"{transcripts}")
+# for file_path, transcript in zip(train_data['path'], train_data['transcription']):
+#     print(f"{file_path}, {transcript}")
+
--- models/models.py
+++ models/models.py
@@ -1,1 +1,11 @@
 import tensorflow as tf
+
+
+class TranformerEncoder(tf.keras.layers.Layer):
+    def __init__(self, num_heads, key_dim, rate=0.1):
+        super(TranformerEncoder, self).__init__()
+        self.mha = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=key_dim)
+        self.layer_norm_1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layer_norm_2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.drop_1 = tf.keras.layers.Dropout(rate=rate)
+        self.drop_2 = tf.keras.layers.Dropout(rate=rate)
