2ae79980afb0ce1656f59341b1d6abb831d056d1
serchsm <serchsm@users.nonreply.github.com>
2021-04-18 21:44:30 -0400

    "[feature] Bert tokenizer for all transcripts"

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,9 @@
-import logging
 import pathlib
+import time
 import pandas as pd
 import tensorflow as tf
+
+from tensorflow_text.tools.wordpiece_vocab import bert_vocab_from_dataset as bert_vocab
 
 tf.get_logger().setLevel('ERROR')
 pwd = pathlib.Path.cwd()
@@ -11,11 +13,16 @@ test_data = pd.read_csv(pwd.joinpath('fluent_speech_commands_dataset', 'data', '
 validation_data = pd.read_csv(pwd.joinpath('fluent_speech_commands_dataset', 'data', 'valid_data.csv'))
 
 all_transcripts = pd.concat([train_data, test_data, validation_data], ignore_index=True)
-
-for transcript in all_transcripts['transcription']:
-    print(f"{transcript}")
-
 fsc_transcripts = tf.data.Dataset.from_tensor_slices(all_transcripts['transcription'].str.lower().values)
+
+bert_tokenizer_params = dict(lower_case=True)
+reserved_tokens = ["[PAD]", "[START]", "[END]", "[UNK]"]
+bert_vocab_args = dict(vocab_size=100,
+                       reserved_tokens=reserved_tokens,
+                       bert_tokenizer_params=bert_tokenizer_params,
+                       learn_params={})
+fsc_vocabulary = bert_vocab.bert_vocab_from_dataset(fsc_transcripts.batch(100).prefetch(2), **bert_vocab_args)
+print(f"{fsc_vocabulary}")
 
 # for transcripts in fsc_transcripts.take(5):
 #     print(f"{transcripts}")
